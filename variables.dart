5862f037c84be563d533c2b121aa8221283866ab
musa <yurtmusa@gmail.com>
2020-10-27 08:43:44 +0000

    Initial commit

---
+++ variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String message = 'Hello Musa';
+  int bornDate = 1988;
+  double age = 31.5;
+  bool isOnline = true;
+
+	print(message);
+  print('Born Date: '+bornDate.toString());
+  print('Age: '+age.toString());
+  print('Is Online: '+isOnline.toString());
+}
